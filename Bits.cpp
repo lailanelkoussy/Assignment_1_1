c83541eb8ed2dd8a5cbf5c5ddc3abe13e1b1bb50
lailanelkoussy <lailanelkoussy@aucegypt.edu>
2018-10-05 19:38:16 +0200

    Created Project and Bits Class

---
+++ Bits.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Laila Nasser ElKoussy 900160812 on 10/5/18.
+//
+
+#include "Bits.h"
---
+++ Bits.h
@@ -0,0 +1,14 @@
+//
+// Created by Laila Nasser ElKoussy 900160812 on 10/5/18.
+//
+
+#ifndef ASSIGNMENT_1_1_BITS_H
+#define ASSIGNMENT_1_1_BITS_H
+
+
+class Bits {
+
+};
+
+
+#endif //ASSIGNMENT_1_1_BITS_H
